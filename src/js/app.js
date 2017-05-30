5daee8e4d56bfbfd2d17e2b9631f665c52708911
Brahim Sourny <brahimsourny@MacBook-Pro-de-Brahim.local>
2017-05-30 23:14:28 +0200

    fix error on the app.js file

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 import '../sass/index.sass';
-import name  from '../js/config';
+import name from '../js/config';
 
-const add =  ( a, b) => a + b;
+const add = (a, b) => a + b;
 /* eslint-disable no-console */
 console.log(add(14, 20));
-export default   add;
+export default add;
 
 /* eslint-disable no-console */
-console.log   (name);
+console.log(name);
 
