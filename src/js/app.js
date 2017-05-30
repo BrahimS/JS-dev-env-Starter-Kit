08c71ae826979ad35cefd64efed9d3b27f4d2bbf
Brahim Sourny <brahimsourny@MacBook-Pro-de-Brahim.local>
2017-05-30 22:59:41 +0200

    add git hook for eslint

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
-
 import '../sass/index.sass';
+import name from '../js/config';
 
 const add = (a, b) => a + b;
 /* eslint-disable no-console */
 console.log(add(14, 20));
+export default add;
 
-export default { add };
+/* eslint-disable no-console */
+console.log(name);
 
---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+// Config file
+const name = 'Brahim';
+export default name;
